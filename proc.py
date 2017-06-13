4b262ea9cd48da88c4b8a0d075ece3ed6a2d6da0
Jun Zhao <zhaojun7987@gmail.com>
2017-06-13 17:33:41 -0400

    lots of updates

--- proc.py
+++ proc.py
@@ -4,24 +4,34 @@ from __future__ import division
 
 import os
 import json
-import glob
 import numpy as np
 import pandas as pd
 import tensorflow as tf
 from csv import QUOTE_NONE as QN
 
 tf.app.flags.DEFINE_string('input', '', 'Input file.')
+tf.app.flags.DEFINE_string('emb_path', '', 'Path to pretrained embs.')
 tf.app.flags.DEFINE_string('data_dir', './data', 'Data directory.')
-tf.app.flags.DEFINE_string('aggregation', 'none', 'Aggregation scheme.')
 tf.app.flags.DEFINE_integer('random_seed', 12358, 'Random seed.')
 tf.app.flags.DEFINE_integer('vocab_size', 0, 'Max vocabulary size.')
 tf.app.flags.DEFINE_integer('vocab_cutoff', 5, 'Cutoff for vocabulary.')
 tf.app.flags.DEFINE_float('test_split', 0., 'Split for testing data.')
 tf.app.flags.DEFINE_float('valid_split', .2, 'Split for validation data.')
 tf.app.flags.DEFINE_bool('weights', False, 'Weighted training samples')
-tf.app.flags.DEFINE_bool('pretrained_embs', False, 'Use word vectors.')
+tf.app.flags.DEFINE_bool('flatten', False, 'Flatten to vectors.')
+tf.app.flags.DEFINE_bool('oov_embs', False, 'Out of vocabulary embs.')
+tf.app.flags.DEFINE_bool('aggregate', False, 'Aggregate weights.')
+tf.app.flags.DEFINE_bool('best_only', False, 'Pick the best category.')
+tf.app.flags.DEFINE_bool('normalize', False, 'Normalize the weights.')
+
 
 FLAGS = tf.app.flags.FLAGS
+
+if FLAGS.flatten:
+    FLAGS.normalize = True
+
+if FLAGS.best_only or FLAGS.normalize:
+    FLAGS.aggregate = True
 
 
 def get_path(fn):
@@ -49,39 +59,55 @@ def get_split(data):
 
 
 def get_vocab(tokens=None):
-    specials = ['<pad>', '<unk>']
     if tokens:
-        vocab = specials
+        vocab = ['</s>', '<unk>']
         for k, v in tokens.iteritems():
             if v >= FLAGS.vocab_cutoff:
                 vocab.append(k)
         if FLAGS.vocab_size > 0 and len(vocab) > FLAGS.vocab_size:
             vocab = vocab[:FLAGS.vocab_size]
-        return vocab
     else:
-        embs = glob.glob(get_path('*.vec'))
-        tokens = []
-
-        if len(embs) == 0:
-            raise Exception('Did not find file with .vec ext.')
-        elif len(embs) > 1:
-            raise Exception('Found more than one file with .vec ext.')
-
-        with open(embs[0]) as f:
+        vocab = []
+        with open(FLAGS.emb_path) as f:
             for i, line in enumerate(f):
-                if i == 0:
-                    emb_size = int(line.split()[1])
-                else:
-                    tokens.append(line.split()[0])
-        return specials + tokens, emb_size
+                if i > 0:
+                    vocab.append(line.split()[0])
+        vocab.insert(1, '<unk>')
+    return vocab
 
 
 def read_data():
     data = pd.read_csv(
-        FLAGS.input, names=get_names(), sep='\t', quoting=QN)
+        FLAGS.input, names=get_names(),
+        sep='\t', quoting=QN, dtype={'label': str})
     if not FLAGS.weights:
         data['weight'] = 1
     return data
+
+
+def proc_embs(vocab):
+    embs = {}
+    with open(FLAGS.emb_path) as f:
+        for i, line in enumerate(f):
+            if i == 0:
+                info = line
+                vocab_size, emb_size = [int(s) for s in line.split()]
+            else:
+                embs[line.split()[0]] = line
+
+    scale = np.sqrt(3) / np.sqrt(vocab_size + 2)
+    with open(get_path('cnncat_embs.vec'), 'w') as f:
+        f.write(info)
+        f.write(embs['</s>'])
+        f.write(' '.join(['<unk>'] + ['0'] * emb_size) + '\n')
+        for w in vocab[2:]:
+            if w in embs:
+                f.write(embs[w])
+            else:
+                ra = np.random.uniform(-scale, scale, emb_size)
+                f.write(' '.join([w] + map(str, ra)))
+
+    return emb_size
 
 
 def init(data):
@@ -90,24 +116,26 @@ def init(data):
         weights.name = 'weight'
         data = weights.to_frame().reset_index()
 
-    tokens = {}
-    labels = set()
+    labels = sorted(set(data.label))
     max_length = data.text.apply(lambda x: len(x.split())).max()
-    for i, r in data.iterrows():
-        labels.add(str(r['label']))
-        for t in r['text'].split():
-            if t in tokens:
-                tokens[t] += r['weight']
-            else:
-                tokens[t] = r['weight']
 
-    if FLAGS.pretrained_embs:
-        vocab, emb_size = get_vocab()
+    if FLAGS.emb_path and FLAGS.oov_embs:
+        vocab = get_vocab()
+        emb_size = proc_embs(vocab)
     else:
+        tokens = {}
+        for i, r in data.iterrows():
+            for t in r['text'].split():
+                if t in tokens:
+                    tokens[t] += r['weight']
+                else:
+                    tokens[t] = r['weight']
         vocab = get_vocab(tokens)
-        emb_size = None
 
-    labels = sorted(labels)
+        if FLAGS.emb_path:
+            emb_size = proc_embs(vocab)
+        else:
+            emb_size = None
 
     write_list(vocab, 'vocab')
     write_list(labels, 'labels')
@@ -115,35 +143,65 @@ def init(data):
 
 
 def aggregate(data):
-    if FLAGS.aggregation not in ['none', 'asis', 'best', 'norm', 'flat']:
-        raise ValueError('Unknown aggregation scheme %s.' % FLAGS.aggregation)
+    weights = data.groupby(['label', 'text']).weight.sum()
+    weights.name = 'weight'
+    data = weights.to_frame().reset_index()
 
-    if FLAGS.aggregation != 'none':
-        weights = data.groupby(['label', 'text']).weight.sum()
-        weights.name = 'weight'
-        data = weights.to_frame().reset_index()
+    if FLAGS.best_only:
+        data.sort_values('weight', ascending=False, inplace=True)
+        data.drop_duplicates('text', inplace=True)
+        data.weight = 1
 
-        if FLAGS.aggregation == 'asis':
-            pass
-        elif FLAGS.aggregation == 'best':
-            data.sort_values('weight', ascending=False, inplace=True)
-            data.drop_duplicates('text', inplace=True)
-            data.weight = 1
-        else:
-            all_weights = data.groupby('text').weight.sum()
-            all_weights.name = 'all_weights'
-            all_weights = all_weights.to_frame().reset_index()
-            data = pd.merge(data, all_weights)
-            data.weight = data.weight / data.all_weights
-            data.drop('all_weights', axis=1, inplace=True)
+    return data
 
-    return data.sample(frac=1.0)
+
+def normalize(data):
+    train = data[data.split == 'train']
+    other = data[data.split != 'train']
+
+    all_weights = train.groupby('text').weight.sum()
+    all_weights.name = 'all_weights'
+    all_weights = all_weights.to_frame().reset_index()
+    train = pd.merge(train, all_weights)
+    train.weight = train.weight / train.all_weights
+    train.drop('all_weights', axis=1, inplace=True)
+
+    train['split'] == 'train'
+
+    return pd.concat([train, other], ignore_index=True)
+
+
+def flatten(data, _labels):
+    train = data[data.split == 'train']
+    other = data[data.split != 'train']
+
+    train = train.pivot(index='text', columns='label', values='weight')
+    train = train.reindex(columns=_labels)
+    train.fillna(value=0, inplace=True)
+
+    texts = []
+    labels = []
+    for t in train.itertuples():
+        texts.append(t[0])
+        labels.append(t[1:])
+
+    train = pd.DataFrame({'text': texts, 'label': labels})
+    train['weight'] = 1
+    train['split'] = 'train'
+
+    return pd.concat([train, other], ignore_index=True)
 
 
 def serialize(label, tokens, length, weight):
     seq = tf.train.SequenceExample()
 
-    seq.context.feature['label'].int64_list.value.append(label)
+    if isinstance(label, tuple):
+        _label = seq.feature_lists.feature_list['label']
+        for l in label:
+            _label.feature.add().float_list.value.append(l)
+    else:
+        seq.context.feature['label'].int64_list.value.append(label)
+
     seq.context.feature['length'].int64_list.value.append(length)
     seq.context.feature['weight'].float_list.value.append(weight)
 
@@ -159,14 +217,20 @@ def write_list(l, kind):
 
 
 def write_records(data, _vocab, _labels, _length):
+    _vocab = {w: i for i, w in enumerate(_vocab)}
+    _labels = {l: i for i, l in enumerate(_labels)}
+
     for k in ['train', 'valid', 'test']:
         writer = tf.python_io.TFRecordWriter(get_path('cnncat.%s.tfr' % k))
         for i, r in data[data.split == k].iterrows():
-            label = _labels.index(str(r['label']))
+            if FLAGS.flatten and k == 'train':
+                label = r['label']
+            else:
+                label = _labels[r['label']]
             tokens = []
             for t in r['text'].split():
                 if t in _vocab:
-                    tokens.append(_vocab.index(t))
+                    tokens.append(_vocab[t])
                 else:
                     tokens.append(1)
 
@@ -192,17 +256,29 @@ if __name__ == '__main__':
         'vocab_size': len(_vocab)
     }
 
-    if FLAGS.pretrained_embs:
+    if FLAGS.emb_path:
         meta['emb_size'] = emb_size
+
+    if FLAGS.aggregate:
+        print('Aggregating...')
+        data = aggregate(data)
 
     print('Splitting data...')
     split = get_split(data)
-
-    print('Aggregating...')
-    data = aggregate(data)
-
     data['split'] = data.text.apply(lambda x: split[x])
+
+    if FLAGS.normalize:
+        print('Normalizing...')
+        data = normalize(data)
+
+    if FLAGS.flatten:
+        print('Flattening...')
+        data = flatten(data, _labels)
+
     meta['train_size'] = data[data.split == 'train'].shape[0]
+
+    print('Shuffling...')
+    data = data.sample(frac=1.0)
 
     print('Writing meta data...')
     with open(get_path('meta.json'), 'w') as f:
